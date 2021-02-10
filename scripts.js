5ef259c28aeba9a37827a2802ec7dda6875aa665
aashuindian <74951741+aashuindian@users.noreply.github.com>
2021-02-10 18:51:26 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function generate_cat() {
+    var img = document.createElement('img');
+    var sis = document.getElementById('flex_cat_container');
+    img.src = "https://thumbs.gfycat.com/FoolhardyVagueAtlanticridleyturtle-small.gif"
+
+    sis.appendChild(img);
+
+}
